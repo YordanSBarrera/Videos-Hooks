7ca5a9ce4b866151ed2703d45c92bd68eb85b2ab
Yordan <yordansaldanabarrera@gmail.com>
2021-01-31 07:43:37 -0300

    actualizando repo

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import useVideos from '../hooks/useVideos';
 const App = () => {
 
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos('Marilyn Mason')
+  const [videos, search] = useVideos('Cuba')
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
